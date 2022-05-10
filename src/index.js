f9f8f1ba911aa09b3295d351cd6d4f6a0dd65edf
marurevi <maryrev@hotmail.com>
2022-05-10 16:39:29 -0300

    Add dinamic table in js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import './style.css';
 
 const tableDisplay = document.querySelector('.dinamic-table');
-tableDisplay.innerHTML= 'Display dinamic table here!';
+tableDisplay.innerHTML= '<ul class="row"></ul>';
 tableDisplay.className= 'dinamicTable';
+
+const table = [
+  {
+    name: 'Mariana',
+    score: 12345
+  },
+  {
+    name: 'Fernando',
+    score: 12345
+  },
+  {
+    name: 'Daniela',
+    score: 12345
+  },
+  {
+    name: 'Julio',
+    score: 12345
+  },
+  {
+    name: 'Viviana',
+    score: 12345
+  },
+  {
+    name: 'Marcos',
+    score: 12345
+  }
+];
+
+table.forEach(user => {
+  const line = document.createElement('li');
+  line.innerHTML= `<div>${user.name}</div><div>${user.score}</div>`;
+  line.className = 'line';
+  line.classList.add('row');
+  tableDisplay.appendChild(line);
+})
